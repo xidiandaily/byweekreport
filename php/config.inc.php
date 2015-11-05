b8abc6775fbe8d77892a613eb46c9d92814c998f
chiyl <xidiandaily@126.com>
2015-11-05 19:39:33 +0800

    Chg:1.修改url
    Signed-off-by: chiyl <xidiandaily@126.com>

--- php/config.inc.php
+++ php/config.inc.php
@@ -7,8 +7,8 @@ if($_envid == 3){
     Loader::$config['adjax_url'] ="http://match.by.com/byweekreport/adjax.php";
     Loader::$config['dbconf']['main'] = array('host'=>'127.0.0.1','port'=>'3306', 'user'=>'byweekuser', 'passwd'=>'passwdbyweekuser', 'dbname'=>'byweekreport');
 }else($_envid == 2){
-    Loader::$config['base_url'] = "http://bytool.com/byweekreport/";
-    Loader::$config['adjax_url'] ="http://bytool.com/byweekreport/adjax.php";
+    Loader::$config['base_url'] = "http://xidiandaily.jios.org:3603/byweekreport/";
+    Loader::$config['adjax_url'] ="http://xidiandaily.jios.org:3603/byweekreport/adjax.php";
     Loader::$config['dbconf']['main'] = array('host'=>'127.0.0.1','port'=>'3306', 'user'=>'byweekuser', 'passwd'=>'passwdbyweekuser', 'dbname'=>'byweekreport');
 }else{
     Loader::$config['base_url'] = "http://bytool.com/byweekreport/";
