44a96453a8c5426f0b51b67dca30561095952ee5
chiyl <xidiandaily@126.com>
2015-11-05 19:44:58 +0800

    Chg:1.修复bug;
    Signed-off-by: chiyl <xidiandaily@126.com>

--- php/config.inc.php
+++ php/config.inc.php
@@ -6,7 +6,7 @@ if($_envid == 3){
     Loader::$config['base_url'] = "http://match.by.com/";
     Loader::$config['adjax_url'] ="http://match.by.com/byweekreport/adjax.php";
     Loader::$config['dbconf']['main'] = array('host'=>'127.0.0.1','port'=>'3306', 'user'=>'byweekuser', 'passwd'=>'passwdbyweekuser', 'dbname'=>'byweekreport');
-}else($_envid == 2){
+}else if($_envid == 2){
     Loader::$config['base_url'] = "http://xidiandaily.jios.org:3603/byweekreport/";
     Loader::$config['adjax_url'] ="http://xidiandaily.jios.org:3603/byweekreport/adjax.php";
     Loader::$config['dbconf']['main'] = array('host'=>'127.0.0.1','port'=>'3306', 'user'=>'byweekuser', 'passwd'=>'passwdbyweekuser', 'dbname'=>'byweekreport');
