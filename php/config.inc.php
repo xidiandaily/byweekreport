84b42ec5a766be7a702051690b278c10ccf42f59
chiyl <xidiandaily@126.com>
2015-11-05 19:33:52 +0800

    Chg:1.更新DB用户名和密码;
    Signed-off-by: chiyl <xidiandaily@126.com>

--- php/config.inc.php
+++ php/config.inc.php
@@ -3,10 +3,13 @@ $_envid=3;
 include 'debug.php';
 
 if($_envid == 3){
+    Loader::$config['base_url'] = "http://match.by.com/";
+    Loader::$config['adjax_url'] ="http://match.by.com/byweekreport/adjax.php";
+    Loader::$config['dbconf']['main'] = array('host'=>'127.0.0.1','port'=>'3306', 'user'=>'byweekuser', 'passwd'=>'passwdbyweekuser', 'dbname'=>'byweekreport');
+}else($_envid == 2){
     Loader::$config['base_url'] = "http://bytool.com/byweekreport/";
     Loader::$config['adjax_url'] ="http://bytool.com/byweekreport/adjax.php";
-    Loader::$config['dbconf']['main'] = array('host'=>'127.0.0.1','port'=>'3306', 'user'=>'root', 'passwd'=>'root', 'dbname'=>'byweekreport');
-
+    Loader::$config['dbconf']['main'] = array('host'=>'127.0.0.1','port'=>'3306', 'user'=>'byweekuser', 'passwd'=>'passwdbyweekuser', 'dbname'=>'byweekreport');
 }else{
     Loader::$config['base_url'] = "http://bytool.com/byweekreport/";
     Loader::$config['adjax_url'] ="http://bytool.com/byweekreport/adjax.php";
